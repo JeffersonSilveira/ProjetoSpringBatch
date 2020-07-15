62e100e19057d72a2c77ebca5ea0efac934b3b00
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-07-14 21:14:38 -0300

    primeiro job

---
+++ src/main/java/br/com/primeiroprojetospringbatch/PrimeiroProjetoSpringBatchApplication.java
@@ -0,0 +1,12 @@
+package br.com.primeiroprojetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringBatchApplication.class, args);
+	}
+}
